79e2d2e87377d93860f20b6f1c491bbfe3baf152
utkuglsvn <utku.glsvn@gmail.com>
2018-12-12 13:23:04 +0300

    corner finding sudoku

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import operator
 
 #show image
 def show_image(img):
@@ -9,23 +10,50 @@ def show_image(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-img = cv2.imread('img/sudoku.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('img/image5.jpg', cv2.IMREAD_GRAYSCALE)
 show_image(img)
 
 #gurultu azaltma
 def pre_process_image(img, skip_dilate=False):
     proc = cv2.GaussianBlur(img.copy(), (9, 9),0)
     proc = cv2.adaptiveThreshold(proc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    proc = cv2.bitwise_not(img, img)
+    proc = cv2.bitwise_not(proc, proc)
     return proc
+
+def findCorners(img):
+    contours, hierarchy = cv2.findContours(processed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    polygon = contours[0]
+    # Largest image    height_px_1 = box[0][1] - box[3][1]
+    bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+    top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+    bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+    top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+	# Return an array of all 4 points using the indices
+	# Each point is in its own array of one coordinate
+    return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
+
+def display_points(in_img, points, radius=5, colour=(0, 0, 255)):
+    img = in_img.copy()
+    if len(colour) == 3:
+        if len(img.shape) == 2:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        elif img.shape[2] == 1:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    for point in points:
+        cv2.circle(img, tuple(int(x) for x in point), 5, (255, 255, 255), -1)
+    show_image(img)
+    return img
 
 processed = pre_process_image(img)
 
 show_image(processed)
 
-#Contours
-ret, thresh = cv2.threshold(img, 127, 255, 0)
-im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#corner finding
+corners = findCorners(processed)
+display_points(processed, corners)
+#ext_contours, hier = cv2.findContours(processed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
 """buna gerek yok suanlik
 #Thresholding
 ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
