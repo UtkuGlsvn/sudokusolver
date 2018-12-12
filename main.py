746ed4bc2a32100077037dfa4d9f6c3f70b2bfd1
utkuglsvn <utku.glsvn@gmail.com>
2018-12-12 14:29:00 +0300

    perspective transform

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def show_image(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-img = cv2.imread('img/image5.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('img/image3.jpg', cv2.IMREAD_GRAYSCALE)
 show_image(img)
 
 #gurultu azaltma
@@ -18,6 +18,9 @@ def pre_process_image(img, skip_dilate=False):
     proc = cv2.GaussianBlur(img.copy(), (9, 9),0)
     proc = cv2.adaptiveThreshold(proc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     proc = cv2.bitwise_not(proc, proc)
+    if not skip_dilate:
+      kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]],np.uint8)
+      proc = cv2.dilate(proc, kernel)
     return proc
 
 def findCorners(img):
@@ -28,12 +31,12 @@ def findCorners(img):
     bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
     top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
     bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+
     top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
 	# Return an array of all 4 points using the indices
-	# Each point is in its own array of one coordinate
     return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
 
-def display_points(in_img, points, radius=5, colour=(0, 0, 255)):
+def display_points(in_img, points, radius=5, colour=(255, 255, 255)):
     img = in_img.copy()
     if len(colour) == 3:
         if len(img.shape) == 2:
@@ -41,31 +44,40 @@ def display_points(in_img, points, radius=5, colour=(0, 0, 255)):
         elif img.shape[2] == 1:
             img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     for point in points:
-        cv2.circle(img, tuple(int(x) for x in point), 5, (255, 255, 255), -1)
+        cv2.circle(img, tuple(int(x) for x in point), radius, colour, -1)
     show_image(img)
     return img
+#sudokoyu ortalama
+def distance_between(p1, p2):
+	a = p2[0] - p1[0]
+	b = p2[1] - p1[1]
+	return np.sqrt((a ** 2) + (b ** 2))
+
+
+def crop_and_warp(img, crop_rect):
+    top_left, top_right, bottom_right, bottom_left = crop_rect[0], crop_rect[1], crop_rect[2], crop_rect[3]
+    src = np.array([top_left, top_right, bottom_right, bottom_left], dtype='float32')
+    side = max([
+		distance_between(bottom_right, top_right),
+		distance_between(top_left, bottom_left),
+		distance_between(bottom_right, bottom_left),
+		distance_between(top_left, top_right)
+	])
+    dst = np.array([[0, 0], [side - 1, 0], [side - 1, side - 1], [0, side - 1]], dtype='float32')
+    m = cv2.getPerspectiveTransform(src, dst)
+    return cv2.warpPerspective(img, m, (int(side), int(side)))
 
 processed = pre_process_image(img)
 
 show_image(processed)
 
 #corner finding
+
 corners = findCorners(processed)
 display_points(processed, corners)
-#ext_contours, hier = cv2.findContours(processed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-"""buna gerek yok suanlik
-#Thresholding
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
-thresh5 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-titles = ['Original Image','BINARY','BINARY_INV','new','TOZERO','TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
-for i in xrange(6):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-plt.show()
-"""
+#sudoku ortalama
+processed = pre_process_image(img)
+corners = findCorners(processed)
+cropped = crop_and_warp(img, corners)
+show_image(cropped)
