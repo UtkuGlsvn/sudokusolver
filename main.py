3d64430c545f464f4130dd8bcdf74d102c3430b1
utkuglsvn <you@example.com>
2018-12-22 15:46:49 +0300

    Sudoku solver add and controll

--- main.py
+++ main.py
@@ -243,6 +243,14 @@ def scale_and_centre(img, size, margin=0, background=0):
 # Importing the required Keras modules containing model and layers
 # Creating a Sequential Model and adding the layers
 
+def checkGrid(grid):
+  for row in range(0,9):
+      for col in range(0,9):
+        if grid[row][col]==0:
+          return False
+
+  #We have a complete grid!
+  return True
 
 def getEveryDigits(img,squares):
     labels = []
@@ -274,16 +282,65 @@ def getEveryDigits(img,squares):
     #
 
 def matrix_convert(label):
-    a=0
-    c=[]
-    for i in range(0,9):
-        c.append(label[a:a+9])
+  a=0
+  matrix=[]
+  for i in range(0,9):
+        matrix.append(label[a:a+9])
         a=a+9
-    for i in range(0,9):
-        print(c[i])
-processed = pre_process_image(img)
+  for i in range(0,9):
+        print(matrix[i])
+  solveGrid(matrix)
+#####
+def solveGrid(grid):
+  #Find next empty cell
+  for i in range(0,81):
+    row=i/9
+    col=i%9
+    if grid[row][col]==0:
+      for value in range (1,10):
+        #Check that this value has not already be used on this row
+        if not(value in grid[row]):
+          #Check that this value has not already be used on this column
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            #Identify which of the 9 squares we are working on
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if checkGrid(grid):
+                print(grid)
+                print("Complete and Checked")
+                return True
+              else:
+                  if solveGrid(grid):
+                    return True
+      break
+  grid[row][col]=0
 
+#####
 #show_image(processed,"pre_process_image")
+processed = pre_process_image(img)
 
 #corner finding
 
