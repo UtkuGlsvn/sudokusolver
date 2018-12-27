1b9c7ec4caab9e05c2313083e5a36be4e7b113aa
utkuglsvn <you@example.com>
2018-12-28 00:22:06 +0300

    Bug resolved, fixed code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from __future__ import print_function
 import cv2
 import argparse
 import numpy as np
-from PIL import Image
+from PIL import ImageFont, ImageDraw, Image
 import operator
 import copy
 import numpy as np
@@ -10,20 +9,21 @@ from keras.preprocessing import image
 import tensorflow as tf
 from skimage.segmentation import clear_border
 from keras.models import load_model
-from load import *
-#show image
+#Show Image
 def show_image(img,title):
-    #cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(title, 450,450)
     cv2.imshow(title, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 ap = argparse.ArgumentParser()
 args = vars(ap.parse_args())
-img = cv2.imread('img/image4.jpg', cv2.IMREAD_GRAYSCALE)
-show_image(img,"title")
 
-#gurultu azaltma
+img = cv2.imread('img/image4.jpg', cv2.IMREAD_GRAYSCALE)
+show_image(img,"Original Image")
+
+#Image filter processing
 def pre_process_image(img, skip_dilate=False):
     proc = cv2.GaussianBlur(img.copy(), (9, 9),0)
     proc = cv2.adaptiveThreshold(proc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)
@@ -33,34 +33,21 @@ def pre_process_image(img, skip_dilate=False):
       proc = cv2.dilate(proc, kernel)
     return proc
 
+#Find image corners
 def findCorners(img):
     h,contours, hierarchy = cv2.findContours(processed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
     polygon = contours[0]
-    # Largest image    height_px_1 = box[0][1] - box[3][1]
+
     bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
     top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
     bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
 
     top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-	# Return an array of all 4 points using the indices
     return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
 
-"""
-def findCorners(img):
-    contours, hierarchy = cv2.findContours(processed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    polygon = contours[0]
-    # Largest image    height_px_1 = box[0][1] - box[3][1]
-    bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-    top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-    bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-
-    top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
-	# Return an array of all 4 points using the indices
-    return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
-"""
-def display_points(in_img, points, radius=5, colour=(255, 255, 255)):
+#Function used to specify point
+def display_points(in_img, points, radius=5, colour=(0, 0, 255)):
     img = in_img.copy()
     if len(colour) == 3:
         if len(img.shape) == 2:
@@ -71,11 +58,13 @@ def display_points(in_img, points, radius=5, colour=(255, 255, 255)):
         cv2.circle(img, tuple(int(x) for x in point), radius, colour, -1)
     show_image(img,"display_points")
     return img
-#sudokoyu ortalama
+
+
 def distance_between(p1, p2):
 	a = p2[0] - p1[0]
 	b = p2[1] - p1[1]
 	return np.sqrt((a ** 2) + (b ** 2))
+
 def display_rects(in_img, rects, colour=255):
     img = in_img.copy()
     for rect in rects:
@@ -97,166 +86,22 @@ def crop_and_warp(img, crop_rect):
     return cv2.warpPerspective(img, m, (int(side), int(side)))
 
 def infer_grid(img):
-	"""Infers 81 cell grid from a square image."""
 	squares = []
 	side = img.shape[:1]
 	side = side[0] / 9
 	for i in range(9):
 		for j in range(9):
-			p1 = (i * side, j * side)  # Top left corner of a bounding box
-			p2 = ((i + 1) * side, (j + 1) * side)  # Bottom right corner of bounding box
+			p1 = (i * side, j * side)
+			p2 = ((i + 1) * side, (j + 1) * side)
 			squares.append((p1, p2))
-			#print(squares)
 	return squares
-
-
-
-
-def cut_from_rect(img, rect):
-	"""Cuts a rectangle from an image using the top left and bottom right points."""
-	return img[int(rect[0][1]):int(rect[1][1]), int(rect[0][0]):int(rect[1][0])]
-
-
-def extract_digit(img, rect, size):
-	"""Extracts a digit (if one exists) from a Sudoku square."""
-
-	digit = cut_from_rect(img, rect)  # Get the digit box from the whole square
-
-	# Use fill feature finding to get the largest feature in middle of the box
-	# Margin used to define an area in the middle we would expect to find a pixel belonging to the digit
-	h, w = digit.shape[:2]
-	margin = int(np.mean([h, w]) / 2.5)
-	_, bbox, seed = find_largest_feature(digit, [margin, margin], [w - margin, h - margin])
-	digit = cut_from_rect(digit, bbox)
-
-	# Scale and pad the digit so that it fits a square of the digit size we're using for machine learning
-	w = bbox[1][0] - bbox[0][0]
-	h = bbox[1][1] - bbox[0][1]
-
-	# Ignore any small bounding boxes
-	if w > 0 and h > 0 and (w * h) > 100 and len(digit) > 0:
-		return scale_and_centre(digit, size, 4)
-	else:
-		return np.zeros((size, size), np.uint8)
-
-def get_digits(img, squares, size):
-	"""Extracts digits from their cells and builds an array"""
-	digits = []
-	img = pre_process_image(img.copy(), skip_dilate=True)
-	for square in squares:
-		digits.append(extract_digit(img, square, size))
-	return digits
-
-
-def find_largest_feature(inp_img, scan_tl=None, scan_br=None):
-	"""
-	Uses the fact the `floodFill` function returns a bounding box of the area it filled to find the biggest
-	connected pixel structure in the image. Fills this structure in white, reducing the rest to black.
-	"""
-	img = inp_img.copy()  # Copy the image, leaving the original untouched
-	height, width = img.shape[:2]
-
-	max_area = 0
-	seed_point = (None, None)
-
-	if scan_tl is None:
-		scan_tl = [0, 0]
-
-	if scan_br is None:
-		scan_br = [width, height]
-
-	# Loop through the image
-	for x in range(scan_tl[0], scan_br[0]):
-		for y in range(scan_tl[1], scan_br[1]):
-			# Only operate on light or white squares
-			if img.item(y, x) == 255 and x < width and y < height:  # Note that .item() appears to take input as y, x
-				area = cv2.floodFill(img, None, (x, y), 64)
-				if area[0] > max_area:  # Gets the maximum bound area which should be the grid
-					max_area = area[0]
-					seed_point = (x, y)
-
-	# Colour everything grey (compensates for features outside of our middle scanning range
-	for x in range(width):
-		for y in range(height):
-			if img.item(y, x) == 255 and x < width and y < height:
-				cv2.floodFill(img, None, (x, y), 64)
-
-	mask = np.zeros((height + 2, width + 2), np.uint8)  # Mask that is 2 pixels bigger than the image
-
-	# Highlight the main feature
-	if all([p is not None for p in seed_point]):
-		cv2.floodFill(img, mask, seed_point, 255)
-
-	top, bottom, left, right = height, 0, width, 0
-
-	for x in range(width):
-		for y in range(height):
-			if img.item(y, x) == 64:  # Hide anything that isn't the main feature
-				cv2.floodFill(img, mask, (x, y), 0)
-
-			# Find the bounding parameters
-			if img.item(y, x) == 255:
-				top = y if y < top else top
-				bottom = y if y > bottom else bottom
-				left = x if x < left else left
-				right = x if x > right else right
-
-	bbox = [[left, top], [right, bottom]]
-	return img, np.array(bbox, dtype='float32'), seed_point
-
-def scale_and_centre(img, size, margin=0, background=0):
-	"""Scales and centres an image onto a new background square."""
-	h, w = img.shape[:2]
-
-	def centre_pad(length):
-		"""Handles centering for a given length that may be odd or even."""
-		if length % 2 == 0:
-			side1 = int((size - length) / 2)
-			side2 = side1
-		else:
-			side1 = int((size - length) / 2)
-			side2 = side1 + 1
-		return side1, side2
-
-	def scale(r, x):
-		return int(r * x)
-
-	if h > w:
-		t_pad = int(margin / 2)
-		b_pad = t_pad
-		ratio = (size - margin) / h
-		w, h = scale(ratio, w), scale(ratio, h)
-		l_pad, r_pad = centre_pad(w)
-	else:
-		l_pad = int(margin / 2)
-		r_pad = l_pad
-		ratio = (size - margin) / w
-		w, h = scale(ratio, w), scale(ratio, h)
-		t_pad, b_pad = centre_pad(h)
-
-	img = cv2.resize(img, (w, h))
-	img = cv2.copyMakeBorder(img, t_pad, b_pad, l_pad, r_pad, cv2.BORDER_CONSTANT, None, background)
-	return cv2.resize(img, (size, size))
-
-# Importing the required Keras modules containing model and layers
-# Creating a Sequential Model and adding the layers
-
-def checkGrid(grid):
-  for row in range(0,9):
-      for col in range(0,9):
-        if grid[row][col]==0:
-          return False
-
-  #We have a complete grid!
-  return True
 
 def getEveryDigits(img,squares):
     labels = []
     centers = []
-    model = 	load_model('models/mnist_keras_cnn_model.h5')
-    #(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    model = load_model('models/mnist_keras_cnn_model.h5')
     img2=img.copy()
-    show_image(img2,"asdgsa")
+    show_image(img2,"TEST")
     height, width = img.shape[:2]
     img2 = Image.fromarray(img2)
     for i in range(81):
@@ -268,24 +113,15 @@ def getEveryDigits(img,squares):
 
         digit = cv2.resize(window,(28,28))
         digit = clear_border(digit)
-       # digit = clear_border(digit)
-        #digit = median(digit, disk(2))
-        #digit = gaussian(digit, sigma=0.09) 
-        #show_image(digit,"digit")        #burasina bak
+
         numPixels = cv2.countNonZero(digit)
         if numPixels<80:
             label=0
         else:
             label2 = model.predict_classes([digit.reshape(1,28,28,1)])
-            #print (str(numPixels) + " ** " + str(label2[0]))
-            label=label2[0] 
-            #if numPixels >= 152 and numPixels <= 155:
-            #	label=1
-            #print(label)
+            label=label2[0]
         labels.append(label)
     return matrix_convert(labels)
-#        show_image(img2,"ada")
-    #
 
 def matrix_convert(label):
   a=0
@@ -298,19 +134,25 @@ def matrix_convert(label):
         print(matrix[i])
   print("---------------------------------------")
   return matrix
-#####
+
+def checkGrid(grid):
+  for row in range(0,9):
+      for col in range(0,9):
+        if grid[row][col]==0:
+          return False
+  return True
+
+
+#Backtracking algorithm
 def solveGrid(grid):
-  #Find next empty cell
+
   for i in range(0,81):
     row=i/9
     col=i%9
     if grid[row][col]==0:
       for value in range (1,10):
-        #Check that this value has not already be used on this row
         if not(value in grid[row]):
-          #Check that this value has not already be used on this column
           if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
-            #Identify which of the 9 squares we are working on
             square=[]
             if row<3:
               if col<3:
@@ -333,14 +175,13 @@ def solveGrid(grid):
                 square=[grid[i][3:6] for i in range(6,9)]
               else:
                 square=[grid[i][6:9] for i in range(6,9)]
-            #Check that this value has not already be used on this 3x3 square
             if not value in (square[0] + square[1] + square[2]):
               grid[row][col]=value
               if checkGrid(grid):
-                print("Sudoku solver")
+                print("Sudoku Result")
                 for i in range(0,9):
                       print(grid[i])
-                print("Complete and Checked")
+                print("CHECK CONTROL ")
                 return grid
               else:
                   if solveGrid(grid):
@@ -353,34 +194,31 @@ def writeImg(solved,old,img,squares):
   bottomLeftCornerOfText = (10,500)
   fontScale              = 3
   fontColor              = (255,255,255)
-  lineType               = 3
-  print(old)
+  lineType               = 2
+  img2 = Image.fromarray(img)
   for i in range(81):
     x1=squares[i][0][0]
     x2=squares[i][1][0]
     y1=squares[i][0][1]
     y2=squares[i][1][1]
-    window=img[x1:x2, y1:y2]
+    window=img[y1:y2,x1:x2]
     if old[i/9][i%9]==0:
-    	cv2.putText(img,str(solved[i/9][i%9]),(x1,y2),font,fontScale,fontColor,lineType)
-  cv2.imshow("test",img)
+        show_image(np.array(img2),"RESULT")
+        k=i/9
+        k=k+1
+        tp=(y1,(k*84))
+        cv2.putText(img,str(solved[i/9][i%9]),tp,font,fontScale,fontColor,lineType)
   cv2.waitKey(0)
-#####
 
-#show_image(processed,"pre_process_image")
+
 processed = pre_process_image(img)
-
-#corner finding
-
 corners = findCorners(processed)
-#display_points(processed, corners)
+display_points(processed, corners)
 
-#sudoku ortalama
 cropped = crop_and_warp(processed, corners)
-#sudoku kare cizme
 squares = infer_grid(cropped)
+
 old= getEveryDigits(cropped,squares)
 solved = solveGrid(copy.deepcopy(old))
+
 writeImg(solved,old,cropped,squares)
-
-
